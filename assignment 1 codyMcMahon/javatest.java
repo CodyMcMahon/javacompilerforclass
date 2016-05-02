30e9a1db04ef3cbbc0e7c394f17b429a651f4521
CodyMcMahon <codyjamesmcmahon@gmail.com>
2016-05-02 09:54:36 -0700

    initializing the git

---
+++ assignment 1 codyMcMahon/javatest.java
@@ -0,0 +1,28 @@
+class testme{
+  public static void main(String[]args){
+    1
+    0
+    0x7
+    int
+    123.55
+    123.55f
+    123d
+    123L
+    0b101011
+    strictfp//asdasdasdasdasdasdasd
+    float
+    >>>
+    'i'
+    r
+    0
+    class
+    0x123
+    1444"/* TESTTTTTTTTTT  /"
+    '\r'
+    "dog:::"
+    /*
+    asdasd
+    **/
+  }
+  <EOF>
+}
